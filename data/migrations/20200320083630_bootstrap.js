76846a9a8d824e258ff72de5a110d59fa758db34
apple <apple@apples-MacBook-Pro-2.local>
2020-03-20 09:07:25 -0700

    set up tables, migrations

--- data/migrations/20200320083630_bootstrap.js
+++ data/migrations/20200320083630_bootstrap.js
@@ -1,8 +1,56 @@
 
 exports.up = function(knex) {
+    return knex.schema
+    .createTable('Projects', tbl => {
+        tbl.increments();
+
+        tbl.string('name', 128).notNullable().unique();
+
+        tbl.string('description', 512);
+
+        tbl.boolean('completed', 10).notNullable().unique();
+    })
+
+    .createTable('Resources', tbl => {
+        tbl.increments();
+
+        tbl.string('name', 128).notNullable().unique();
+
+        tbl.string('description', 512);
+
+        tbl.integer('project_id', 10).unsigned().notNullable().references("id").inTable("Projects");
+    })
   
+
+    .createTable('Tasks', tbl => {
+        tbl.increments();
+
+        tbl.string('name', 128).notNullable().unique();
+
+        tbl.string('description', 512).notNullable();
+
+        tbl.boolean('completed', 10).notNullable().unique();
+
+        tbl.integer('project_id', 10).unsigned().notNullable().references("id").inTable("Projects");
+
+        tbl.string('notes', 512);
+    })
+
+
+    .createTable('Project-Task', tbl => {
+        //double primary key
+      tbl.primary(["project_id", "task_id"])
+  
+      tbl.integer('project_id', 128).unsigned().notNullable().references("id").inTable("project");
+  
+      tbl.integer('task_id', 128).unsigned().notNullable().references("id").inTable("task");
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+  .dropTableIfExists("Projects")
+  .dropTableIfExists("Resources")
+  .dropTableIfExists("Tasks")
+  .dropTableIfExists("Project-Task");
 };
